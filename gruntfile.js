f1b9ce506fbb82359ef11839d1bb276045502e9b
Morgan <info@morganfeeney.com>
2019-07-14 11:49:19 +0100

    Initial setup of test repo

---
+++ gruntfile.js
@@ -0,0 +1,54 @@
+const sass = require('node-sass');
+const postcssPresetEnv = require('postcss-preset-env');
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    sass: {
+      options: {
+        implementation: sass,
+        sourcemap: false,
+        outputStyle: "expanded",
+      },
+      test: {
+        files: [
+          {
+            src: "src/scss/style.scss",
+            dest: "dest/css/style.css",
+          }
+        ]
+      },
+    },
+    postcss: {
+      options: {
+        processors: [
+          postcssPresetEnv({
+            stage: 0,
+            autoprefixer: {
+              grid: true,
+            },
+          }),
+        ],
+      },
+      dist: {
+        src: "dest/css/style.css"
+      },
+    },
+    clean: {
+      css: {
+        src: "dest/css/style.css"
+      },
+    },
+  });
+
+  // Load the plugins to run your tasks
+  require("load-grunt-tasks")(grunt, {
+    scope: "devDependencies"
+  });
+
+  // Default task(s).
+  grunt.registerTask("default", [
+    "clean",
+    "sass",
+    "postcss",
+  ]);
+}
